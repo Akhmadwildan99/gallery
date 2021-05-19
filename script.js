b8e0d21c060134bffa240216e7fa4da6b2c5c830
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-05-19 21:25:41 +0700

    belajar buat gallery

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.thumb');
+
+
+container.addEventListener('click', function(e){
+    if(e.target.className == '.thumb');
+    jumbo.src = e.target.src;
+    jumbo.classList.add('fade');
+    setTimeout(function(){
+        jumbo.classList.remove('fade');
+    },500);
+
+    thumbs.forEach(function(thumb) {
+        // if(thumb.classList.contains('active')) {
+        //     thumb.classList.remove('active');
+        // }
+
+        thumb.className = 'thumb';
+    });
+
+    e.target.classList.add('active');
+}); // cara jika nama class sama
+
+
+// const pilihan = document.querySelectorAll('.thumbnail img');
+// pilihan.forEach(function(pil){
+//     pil.addEventListener('click', function(){
+//         const pilihanImg = pil.className;
+//         const jumboPhoto = document.querySelector('.jumbo');
+
+//         jumboPhoto.setAttribute('src', 'img/'+pilihanImg+'.jpg');
+//     })
+
+// });  // cara ini digunakan jika nama class berbeda
+
+
 // const container = document.querySelector('.container');
 // const jumbo = document.querySelector('.jumbo');
 
-// container.addEventListener('click', function(e){
-//     if(e.target.className == '.thumb');
-//     jumbo.src = e.target.src;
-// }); // cara jika nama class sama
-
-
-const pilihan = document.querySelectorAll('.thumbnail img');
-pilihan.forEach(function(pil){
-    pil.addEventListener('click', function(){
-        const pilihanImg = pil.className;
-        const jumboPhoto = document.querySelector('.jumbo');
-
-        jumboPhoto.setAttribute('src', 'img/'+pilihanImg+'.jpg');
-    })
-
-});  // cara ini digunakan jika nama class berbeda
+// container.addEventListener('click', function(e) {
+//     if(e.target.className == '.thumb'){
+//         jumbo.src = e.target.src;
+//         // jumbo.classList.add('fade');
+//     }
+    
+// });
 
